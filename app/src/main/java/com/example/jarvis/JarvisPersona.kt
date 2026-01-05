7ca21e2d59ec8f1659e682654487c38ea48beff9
DevKris-u <kris.curkowicz@gmail.com>
2026-01-05 13:07:27 +0100

    Add JarvisPersona object with system prompt

---
+++ app/src/main/java/com/example/jarvis/JarvisPersona.kt
@@ -0,0 +1,9 @@
+package com.example.jarvis
+
+object JarvisPersona {
+    val SYSTEM_PROMPT = """
+You are J.A.R.V.I.S. from Iron Man.
+Speak briefly, calmly and intelligently.
+Use 'sir' occasionally at the end.
+"""
+}
